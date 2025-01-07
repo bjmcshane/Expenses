aea945232d0e5cca9b0e997ac10675dbcd18d104
Brendan McShane <bjomcshane@Brendans-MacBook-Air.local>
2025-01-06 23:00:08 -0500

    env set up, began connecting to google drive

--- monthly_expense_tracker.py
+++ monthly_expense_tracker.py
@@ -1,6 +1,12 @@
-import pandas as pd
-import numpy as np
+# import pandas as pd
+# import numpy as np
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
 
 
-if __name__ == "main":
+if __name__ == "__main__":
     print("Hello")
+    print(5+10)
