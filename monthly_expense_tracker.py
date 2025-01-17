8c4e39f6357ee6c3c3811ff2ee7f00b88174e589
Brendan <bjomcshane@gmail.com>
2025-01-17 14:35:59 -0500

    added spending on dummy data

--- monthly_expense_tracker.py
+++ monthly_expense_tracker.py
@@ -9,16 +9,17 @@ from googleapiclient.errors import HttpError
 
 if __name__ == "__main__":
     columns=[
-        'year', 'month', 'date', 'money_put_towards_retirement', 'money_saved_for_retirement', 'money_put_towards_house', 'money_saved_for_house', 'money_put_towards_portfolio',
-        'money_saved_in_portfolio'
+        'year', 'month', 'date',
+        'money_spent_on_eating_out', 'money_spent_on_groceries', 'money_spent_entertainment_alcohol',
+        'money_put_towards_retirement', 'money_saved_for_retirement', 'money_put_towards_house', 'money_saved_for_house', 'money_put_towards_portfolio', 'money_saved_in_portfolio'
     ]
     df = pd.DataFrame(columns=columns)
     for row in [
-        [2025, 1, "2025-01-31",  420, 10000, 1200, 10000, 100, 500],
-        [2025, 2, "2025-02-28",  420, 10420, 1200, 11200, 100, 600],
-        [2025, 3, "2025-03-31",  420, 10840, 1200, 12400, 100, 700],
-        [2025, 4, "2025-04-30",  420, 11080, 1200, 13600, 100, 800],
-        [2025, 5, "2025-05-31",  420, 11500, 1200, 14800, 100, 900],
+        [2025, 1, "2025-01-31",  200, 400, 100, 420, 10000, 1200, 10000, 100, 500],
+        [2025, 2, "2025-02-28",  150, 350, 80, 420, 10420, 1200, 11200, 100, 600],
+        [2025, 3, "2025-03-31",  250, 250, 200, 420, 10840, 1200, 12400, 100, 700],
+        [2025, 4, "2025-04-30",  320, 300, 50, 420, 11080, 1200, 13600, 100, 800],
+        [2025, 5, "2025-05-31",  100, 150, 150, 420, 11500, 1200, 14800, 100, 900],
     ]:
         temp = {}
         for val, col in zip(row, columns):
